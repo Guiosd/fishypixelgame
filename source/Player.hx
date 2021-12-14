77386fb4bce6b905a2aad2f3abf1e87feaf569d1
Guiosd <53371875+Guiosd@users.noreply.github.com>
2021-12-14 02:34:11 +0000

    INTI COMMIT

--- source/Player.hx
+++ source/Player.hx
@@ -5,6 +5,6 @@ import flixel.FlxSprite;
 class Player extends FlxSprite {
   public function new(xPos:Int = 0, yPos:Int = 0){
     super(xPos, yPos);
-    loadGraphic("assets/images/FISHY", true, 32, 48)
+    loadGraphic("assets/images/FISHY.png", true, 32, 48)
   }
 }
